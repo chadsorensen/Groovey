d66147ca18704cadfeab537e4a1c2d9b789a7d47
Chad Sorensen <chdsorensen@gmail.com>
2011-05-02 17:43:38 -0700

    initial commit

---
+++ application.rb
@@ -0,0 +1,24 @@
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require File.join(File.dirname(__FILE__), 'environment')
+
+configure do
+  set :views, "#{File.dirname(__FILE__)}/views"
+end
+
+error do
+  e = request.env['sinatra.error']
+  Kernel.puts e.backtrace.join("\n")
+  'Application error'
+end
+
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+# root page
+get '/' do
+  haml :root
+end
